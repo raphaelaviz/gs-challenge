4bbf8faf52172f184589d874b1cd35fc802f9209
rapharrrr <125589553+rapharrrr@users.noreply.github.com>
2024-09-08 18:24:47 +0100

    improved responsiveness on buttons section

--- components/buttons-section.tsx
+++ components/buttons-section.tsx
@@ -69,7 +69,7 @@ const ButtonsSection = ({ taskId, taskScore }: ButtonsSectionProps) => {
 	}
 
 	return (
-		<div className="flex justify-between">
+		<div className="flex flex-col-reverse justify-between sm:flex-row">
 			<Button
 				variant="ghost"
 				className="px-2 py-1 text-xs text-blue-600 hover:text-blue-700 sm:px-3"
@@ -77,7 +77,7 @@ const ButtonsSection = ({ taskId, taskScore }: ButtonsSectionProps) => {
 			>
 				<X className="mr-1 h-4 w-4 sm:h-5 sm:w-5" /> Skip task
 			</Button>
-			<div className="flex space-x-2">
+			<div className="flex flex-col-reverse gap-2 sm:flex-row">
 				<Button
 					className="flex items-center bg-blue-500 px-2 py-1 text-xs hover:bg-blue-600 sm:px-3"
 					onClick={() => handleStatusUpdate('escalate')}
